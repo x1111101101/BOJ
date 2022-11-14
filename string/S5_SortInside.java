e6aed5da79a54e35a620c32322414334dcf18bdf
x1111101101 <97117787+x1111101101@users.noreply.github.com>
2022-11-14 21:16:45 +0900

    Create S5_SortInside.java

---
+++ string/S5_SortInside.java
@@ -0,0 +1,30 @@
+package project;
+
+import java.io.*;
+import java.util.*;
+
+import static java.lang.Math.*;
+import static java.lang.Integer.*;
+import static java.lang.Character.*;
+
+/**
+1427번: 소트 인사이드
+아스키코드에서 숫자들은 이미 정렬된 상태기 때문에 파싱을 하지 않아도 
+*/
+public class Main {
+
+    
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr = br.readLine().toCharArray();
+        Arrays.sort(arr);
+        for(int i = 0; i<arr.length/2; i++) {
+            char t = arr[i];
+            arr[i] = arr[arr.length-i-1];
+            arr[arr.length-i-1] = t;
+        }
+        System.out.println(new String(arr));
+    }
+
+
+}
