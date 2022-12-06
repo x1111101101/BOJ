c21a47a61f047370e271a84e0ce4208c32e12159
x1111101101 <97117787+x1111101101@users.noreply.github.com>
2022-12-06 19:49:03 +0900

    위상정렬 학습 후 작성한 코드

--- graph/G3_줄세우기.java
+++ graph/G3_줄세우기.java
@@ -1,3 +1,5 @@
+package project;
+
 import java.io.*;
 import java.util.*;
 
@@ -6,80 +8,49 @@ import static java.lang.Integer.*;
 import static java.lang.Character.*;
 
 /**
-2252번: 줄세우기
-위상 정렬로 간단히 풀 수 있는 문제지만 위상정렬을 학습하지 않은 상태로 풀어서 메모리 초과가 발생했다.
+2252번: 줄 세우기
+위상정렬을 학습해서 다시 풀었다. 큐의 사이즈가 필요 이상으로 커지지 않아 메모리 초과가 발생하지 않았다.
+위상정렬 알고리즘은 매우 직관적이라서 이해하는데 크게 어려움이 없었다.
+
+degree <= 0이 아닌 degree == 0로 진입차수 체크를 해서 중복 출력을 피했다.
+
+https://www.youtube.com/watch?v=qzfeVeajuyc
+위상정렬 알고리즘은 이 영상을 보며 학습했다.
 */
 public class Main {
-    
-    final static int MULTIPLIER = 100000;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String nums[] = br.readLine().split(" ");
         int n = parseInt(nums[0]);
         int m = parseInt(nums[1]);
-        HashSet<Integer> smaller = new HashSet<>(n);
-        boolean def[] = new boolean[n+1];
+        HashMap<Integer, List<Integer>> bigger = new HashMap<>(n);
+        int degree[] = new int[n+1];
         while(m-- > 0) {
             String sp[] = br.readLine().split(" ");
             int a = parseInt(sp[0]);
             int b = parseInt(sp[1]);
-            smaller.add(a*MULTIPLIER+b);
-            def[a] = true;
-            def[b] = true;
+            degree[b]++;
+            bigger.computeIfAbsent(a, k->new LinkedList<>()).add(b);
         }
         LinkedList<Integer> li = new LinkedList<>();
-        LinkedList<Integer> toAdd = new LinkedList();
-        for(int i = 1; i<=n; i++) {
-            if(!def[i]) continue;
-            int idx = -1;
-            int k = 0;
-            for(int b : li) {
-                if(smaller.contains(i*MULTIPLIER+b)) {
-                    idx = k;
-                    break;
-                }
-                k++;
-            }
-            if(idx == -1) {
-                li.add(i);
-                continue;
-            }
-            li.add(idx, i);
-            Iterator<Integer> iter = li.iterator();
-            for(int p = 0; p<=idx; p++) {
-                iter.next();
-            }
-            k = idx+1;
-            // 8 5 6 7
-            toAdd.clear();
-            while(iter.hasNext()) {
-                int b = iter.next();
-                if(smaller.contains(b*MULTIPLIER+i)) {
-                    iter.remove();
-                    toAdd.addFirst(b);
-                }
-                k++;
-            }
-            for(int r : toAdd) {
-                li.add(idx, r);
-            }
-        }
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for(int v : li) {
-            bw.write(String.valueOf(v));
-            bw.write(' ');
-        }
         for(int i = 1; i<=n; i++) {
-            if(!def[i]) {
-                bw.write(String.valueOf(i));
-                bw.write(' ');
+            if(degree[i] == 0) li.add(i);
+        }
+        while(!li.isEmpty()) {
+            int k = li.removeFirst();
+            bw.write(String.valueOf(k));
+            bw.write(' ');
+            List<Integer> talls = bigger.get(k);
+            if(talls == null) continue;
+            for(int t : talls) {
+                degree[t]--;
+                if(degree[t] == 0) {
+                    li.add(t);
+                }
             }
         }
         bw.close();
     }
-    
-
-    
-    
 }
