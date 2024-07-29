ab947d627584992fac08a160c9dc5138d01b716b
x1111101101 <97117787+x1111101101@users.noreply.github.com>
2024-07-29 15:26:08 +0900

    Update G3_수상 택시.kt

--- sweeping/G3_수상 택시.kt
+++ sweeping/G3_수상 택시.kt
@@ -4,19 +4,13 @@ import kotlin.math.min
 2836번: 수상택시
 */
 
-data class Node(val loc: Int, val id: Int) : Comparable<Node> {
-    override fun compareTo(other: Node): Int {
-        return loc.compareTo(other.loc)
-    }
-}
-
-data class Boundary(val start: Int, val end: Int) {
+data class Boundary(val start: Int, val end: Int): Comparable<Boundary> {
     fun merge(other: Boundary): Boundary? {
         if (other.start !in start..end && other.end !in start..end) return null
         return Boundary(min(start, other.start), max(end, other.end))
     }
-
     fun reversed() = Boundary(end, start)
+    override fun compareTo(other: Boundary): Int = start.compareTo(other.start)
 }
 
 fun main(args: Array<String>): Unit = with(System.`in`.bufferedReader()) {
@@ -26,21 +20,17 @@ fun main(args: Array<String>): Unit = with(System.`in`.bufferedReader()) {
             .split(" ")
             .map { it.toInt() }
             .let { Boundary(it[0], it[1]).reversed() }
-    }.filter { it.end > it.start }
-    if (passengers.isEmpty()) {
-        println(m)
-        return@with
-    }
-    var nodes = passengers.mapIndexed { index, it -> Node(it.start, index) }
+        }
+        .filter { it.end > it.start }
         .sorted()
     val boundaries = ArrayList<Boundary>()
-    boundaries.add(passengers[nodes[0].id])
-    for (i in 1 until nodes.size) {
-        val cur = nodes[i]
+    boundaries.add(passengers[0])
+    for (i in 1 until passengers.size) {
+        val cur = passengers[i]
         val last = boundaries.last()
-        val merged = last.merge(passengers[cur.id])
+        val merged = last.merge(cur)
         if (merged == null) {
-            boundaries.add(passengers[cur.id])
+            boundaries.add(cur)
         } else {
             boundaries[boundaries.lastIndex] = merged
         }
